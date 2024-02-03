8a6c43be40fe519865fbe4aee963f26f39e0bab3
nicole <nicolekaneshige@gmail.com>
2024-02-03 15:44:19 -0800

    Get basic display of board

--- chessMain.py
+++ chessMain.py
@@ -27,12 +27,48 @@ def loadImages():
 '''
 Main driver for our code. Will handle user input and updating the graphics
 '''
-
 def main():
     screen = p.display.set_mode((WIDTH,HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = chessEngine.GameState()
-    print(gs.board)
+    loadImages() #only do this once, before the while loop
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
 
-main()
+'''
+Responsible for all the graphics within current game state. 
+'''
+def drawGameState(screen, gs):
+    drawBoard(screen) 
+    #add in piece highlighting or move suggestions (later)
+    drawPieces(screen, gs.board)
+
+'''
+Draw the squares on the board. The top left square is always light
+'''
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("dark green")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+'''
+Draw pieces on top of the squares on the board using the current GameState.board
+'''
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #if piece is not empty want to draw image
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == "__main__":
+    main()
